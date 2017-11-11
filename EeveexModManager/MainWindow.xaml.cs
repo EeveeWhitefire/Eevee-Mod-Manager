b6525c61623a7846f0918aab093bcc6433b6aafb
Tal Hadad <tal.hadad2001@users.noreply.github.com>
2017-11-11 22:51:03 +0200

    ?

--- EeveexModManager/MainWindow.xaml.cs
+++ EeveexModManager/MainWindow.xaml.cs
@@ -13,6 +13,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using EeveexModManager.Classes;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.IO.Compression;
+using System.IO;
 
 namespace EeveexModManager
 {
@@ -21,35 +25,77 @@ namespace EeveexModManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        string skyrimDir = @"E:\Steam-Main\steamapps\common\Skyrim Special Edition\SkyrimSELauncher.exe";
         public MainWindow()
         {
             InitializeComponent();
-
-            List<Mod> mods = new List<Mod>
-            {
-                new Mod {Active=false, Id=9999, Installed=false, Name="test", Version="6.9.4"},
-                new Mod {Active=false, Id=9999, Installed=false, Name="test", Version="6.9.4"},
-                new Mod {Active=false, Id=9999, Installed=false, Name="test", Version="6.9.4"},
-                new Mod {Active=false, Id=9999, Installed=false, Name="test", Version="6.9.4"},
-            };
-            mods.ForEach(mod =>
-            {
-                var x = new StackPanel
-                {
-                    Orientation = Orientation.Horizontal
-                };
-                var z = new BitmapImage(new Uri("pack://application:,,,/Images/test.png"));
-                
-                x.Children.Add(new Image() { Source = z, Width=20});
-                x.Children.Add(new TextBlock() { Text = $"{mod.Name}        Active:{mod.Active}     Id:{mod.Id}     Version:{mod.Version}" });
-
-                ModelsAndTexturesItem.Items.Add(x);
-            });
+            
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void GameLaunchButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Clicked on launch");
+            if (GamePicker.SelectedIndex == 0)
+            {
+                Process skyrimProcess = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo()
+                {
+                    FileName = skyrimDir,
+                    UseShellExecute = false
+                };
+                skyrimProcess.StartInfo = startInfo;
+                skyrimProcess.Start();
+                skyrimProcess.WaitForExit();
+                MessageBox.Show("exited");
+            }
+        }
+
+        void AddModToCategory(int index, Mod mod)
+        {
+            var x = new StackPanel
+            {
+                Orientation = Orientation.Horizontal
+            };
+            var z = new BitmapImage(new Uri("pack://application:,,,/Images/test.png"));
+
+            x.Children.Add(new Image() { Source = z, Width = 20 });
+            x.Children.Add(new TextBlock() { Text = $"{mod.Name}        Active:{mod.Active}     Id:{mod.Id}     Version:{mod.Version}" });
+
+            ModelsAndTexturesItem.Items.Add(x);
+        }
+
+        private void GameDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.ShowDialog();
+
+            skyrimDir = dialog.FileName;
+        }
+
+        private void InstallModButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.ShowDialog();
+
+            var filePath = dialog.FileName;
+            var fileName = filePath.Split('\\').Last();
+            var modProperties = fileName.Replace(".zip", "").Split('-');
+
+            if (!Directory.Exists($@"Mods\{fileName}"))
+            {
+                ZipFile.ExtractToDirectory(filePath, $@"Mods\{fileName}");
+
+                Mod newMod = new Mod()
+                {
+                    Active = false,
+                    Id = Convert.ToUInt64(modProperties[1]),
+                    Name = modProperties.First(),
+                    Installed = true,
+                    Version = modProperties.Last()
+                };
+
+                AddModToCategory(0, newMod);
+            }
         }
     }
 }
